7ce63a871353dcfe5c672e3e5c7e906c8edd49eb
CasparXL <976301251@qq.com>
2021-04-02 14:53:35 +0800

    更新app安装包

--- app/src/main/java/com/caspar/xl/ui/fragment/MineFragment.kt
+++ app/src/main/java/com/caspar/xl/ui/fragment/MineFragment.kt
@@ -1,11 +1,8 @@
 package com.caspar.xl.ui.fragment
 
-import android.content.Context
 import android.os.Bundle
 import com.caspar.base.base.BaseFragment
 import com.caspar.base.helper.LogUtil
-import com.caspar.xl.R
-import com.caspar.xl.databinding.FragmentHomeBinding
 import com.caspar.xl.databinding.FragmentMineBinding
 
 /**
