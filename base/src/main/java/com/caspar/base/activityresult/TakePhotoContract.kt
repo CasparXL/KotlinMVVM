54ddf7873fa51ad0242a29d8690368dc962047d9
Caspar <Caspar_XL@163.com>
2022-03-21 16:24:41 +0800

    移除部分无用类，新增拍照，选择相册的协定方法

---
+++ base/src/main/java/com/caspar/base/activityresult/TakePhotoContract.kt
@@ -0,0 +1,61 @@
+package com.caspar.base.activityresult
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.annotation.CallSuper
+import androidx.core.content.FileProvider
+import androidx.core.content.contentValuesOf
+import com.caspar.base.helper.LogUtil
+import java.io.File
+
+/**
+ * 拍照协定
+ * Input type  : Unit? 不需要传值
+ * Output type : Uri?  拍照完成后的uri
+ *
+ *  val takePhoto = registerForActivityResult(TakePhotoContract()) { uri: Uri? ->
+ *                  if (uri != null) {
+ *                          if (needCrop) {
+ *                               cropPhoto.launch(CropParams(uri))
+ *                              } else {
+ *                               ivImage.setImageURI(uri)
+ *                          }
+ *                      }
+ *                  }
+ */
+class TakePhotoContract : ActivityResultContract<Unit?, Uri?>() {
+
+    private var uri: Uri? = null
+
+    @CallSuper
+    override fun createIntent(context: Context, input: Unit?): Intent {
+        val mimeType = "image/jpeg"
+        val fileName = "${System.currentTimeMillis()}.jpg"
+        uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            // Android 10 及以上获取图片uri
+            val values = contentValuesOf(
+                Pair(MediaStore.MediaColumns.DISPLAY_NAME, fileName),
+                Pair(MediaStore.MediaColumns.MIME_TYPE, mimeType),
+                Pair(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+            )
+            context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        } else {
+            // Android 9 及以下获取图片uri
+            FileProvider.getUriForFile(
+                context, "${context.packageName}.fileprovider",
+                File(context.externalCacheDir!!.absolutePath, fileName)
+            )
+        }
+        return Intent(MediaStore.ACTION_IMAGE_CAPTURE).putExtra(MediaStore.EXTRA_OUTPUT, uri)
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+        LogUtil.d("take photo uri : $uri")
+        return uri
+    }
+}
--- base/src/main/java/com/caspar/base/utils/test.kt
+++
@@ -1,2 +0,0 @@
-package com.caspar.base.utils
-
