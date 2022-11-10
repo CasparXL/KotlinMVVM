e102a53addc048a0467b6aead7688e5219821e02
CasparXL <Caspar_XL@163.com>
2022-11-10 21:47:35 +0800

    更新刷新列表UI方式，使UI刷新效率更高，完全达到部分UI刷新的效果

--- app/src/main/java/com/caspar/xl/ui/activity/RefreshListActivity.kt
+++ app/src/main/java/com/caspar/xl/ui/activity/RefreshListActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.lifecycleScope
 import com.caspar.base.base.BaseActivity
 import com.caspar.base.utils.log.LogUtil
 import com.caspar.xl.databinding.ActivityRefreshListBinding
+import com.caspar.xl.network.util.GsonUtils
 import com.caspar.xl.ui.adapter.MessageListBean
 import com.caspar.xl.ui.adapter.RefreshListAdapter
 import com.caspar.xl.viewmodel.RefreshListViewModel
@@ -18,6 +19,7 @@ import kotlin.time.measureTime
 /**
  * DiffUtil刷新UI功能
  * 提高UI刷新速度
+ * 不论是什么顺序的数据,都可能存在问题，根据需求自行调整，规避最严重的bug即可,只要数据出现的频率不是很大，那么很多时候刷新页面数据就能解决当前已有的所有问题
  */
 class RefreshListActivity : BaseActivity<ActivityRefreshListBinding>() {
     val mAdapter: RefreshListAdapter = RefreshListAdapter()
@@ -27,6 +29,15 @@ class RefreshListActivity : BaseActivity<ActivityRefreshListBinding>() {
         mBindingView.title.tvCenter.text = "适配器高效刷新"
         mBindingView.title.tvLeft.setOnClickListener {
             finish()
+        }
+        mBindingView.btnChanged.setOnClickListener {
+            mViewModel.revered = !mViewModel.revered
+            mBindingView.btnChanged.text = "数据逆反顺序切换，切刷新加载逻辑也变化(现在:${if (mViewModel.revered) "从大到小" else "从小到大"})"
+            if (mViewModel.revered) {
+                toast("新数据顶在第二页的前面,所以第二页出现重复数据，移除部分重复内容")
+            } else {
+                toast("新数据会直接顶到最后面")
+            }
         }
         mBindingView.btnRefresh.setOnClickListener {
             mViewModel.pageNo = 2
@@ -46,14 +57,7 @@ class RefreshListActivity : BaseActivity<ActivityRefreshListBinding>() {
 
             override fun onLoadMore(refreshLayout: RefreshLayout) {
                 //页数不够，则上拉加载时重新加载第二页
-                if (mAdapter.itemCount < mViewModel.pageNo * mViewModel.pageSize) {
-                    mViewModel.pageNo -= 1
-                    if (mViewModel.pageNo < 1) {
-                        mViewModel.pageNo = 1
-                    }
-                } else {
-                    mViewModel.pageNo += 1
-                }
+                mViewModel.pageNo += 1
                 mViewModel.messageList()
             }
         })
@@ -62,7 +66,10 @@ class RefreshListActivity : BaseActivity<ActivityRefreshListBinding>() {
         mAdapter.setDiffCallback(RefreshListAdapter.DiffDemoCallback())
         mBindingView.rvList.adapter = mAdapter
         mAdapter.setOnItemClickListener { a, v, p ->
-            //更新单个数据这么操作即可
+            //更新单个数据这么操作即可,
+            //更多时候，单个数据更新是最合理的,点击进入详情界面，
+            //做完操作以后，从详情请求最新数据，带回来到当前Item更新数据,这个时候只刷新当前Item的数据，即可达到最新状态
+            //若当前设备详情查询不到,则代表已被移除,回来时移除当前Item即可
             mAdapter.setData(p, mAdapter.data[p].copy(name = "$p"))
         }
     }
@@ -71,6 +78,12 @@ class RefreshListActivity : BaseActivity<ActivityRefreshListBinding>() {
     private fun initObserver() {
         lifecycleScope.launch {
             mViewModel.messageEvent.collect {
+                //没有更多数据时，pageNo-1,不刷新列表
+                if (it.isEmpty() && mViewModel.pageNo != 1){
+                    mViewModel.pageNo --
+                    return@collect
+                }
+                //新增数据不是在列表最前方的话(即最新数据在列表最顶部的情况(当前时间的数据在最顶部))，该方法可以高效刷新当前页或者加载更多数据
                 measureTime {
                     mBindingView.srlRefresh.finishRefresh()
                     mBindingView.srlRefresh.finishLoadMore()
@@ -88,11 +101,18 @@ class RefreshListActivity : BaseActivity<ActivityRefreshListBinding>() {
                         }
                         //所有数据重新叠加
                         val emptyList = mutableListOf<MessageListBean>()
-                        list.map {
-                            emptyList.addAll(it)
+                        list.map { allData ->
+                            emptyList.addAll(allData)
                         }
+                        //当排序是反方向时,先排序反方向，然后筛选掉旧数据，最终在反方向回来
+                        val finalList = emptyList.reversed().distinctBy { db-> db.id }.reversed().toMutableList()
                         //刷新UI
-                        mAdapter.setDiffNewData(emptyList)
+                        mAdapter.setDiffNewData(finalList)
+                        //最终列表没有达到当前页需要的数量时，pageNo - 1
+                        // (正常来说从大到小的顺序时(即时间顺序从现在到以前)，第二页出现了重复数据，才有可能减少pageNo的情况)
+                        if (finalList.size < mViewModel.pageNo * mViewModel.pageSize){
+                            mViewModel.pageNo --
+                        }
                     }
                 }.let {
                     LogUtil.d("加载时间 -> ${it.inWholeMilliseconds} 毫秒")
--- app/src/main/java/com/caspar/xl/viewmodel/RefreshListViewModel.kt
+++ app/src/main/java/com/caspar/xl/viewmodel/RefreshListViewModel.kt
@@ -11,18 +11,40 @@ import kotlin.random.Random
 class RefreshListViewModel : ViewModel() {
     var pageNo: Int = 1
     val pageSize: Int = 10
+    //是否反方向数据
+    var revered: Boolean = false
+    var reveredNumber:Int = -4
     private val _messageEvent: MutableSharedFlow<List<MessageListBean>> = MutableSharedFlow()
     val messageEvent = _messageEvent.asSharedFlow()
+
     fun messageList() {
         viewModelScope.launch {
             val list = mutableListOf<MessageListBean>()
-            repeat(10){
-                val number = ((pageNo - 1) * pageSize) + it + 1
-                //新数据会全部加入，旧数据会只刷新部分UI，刷新UI的效率会极高
-                if (it % 2 == 0){
-                    list.add(MessageListBean(id = number, name = "${number + Random.nextInt(30)}", age = number))
-                } else {
+            if (!revered){
+                repeat(10){
+                    val number = ((pageNo - 1) * pageSize) + it + 1
+                    //新数据会全部加入，旧数据会只刷新部分UI，刷新UI的效率会极高
                     list.add(MessageListBean(id = number, name = "${number}", age = number))
+                }
+            } else {
+                if (pageNo == 1){
+                    repeat(10){
+                        val number = ((pageNo - 1) * pageSize) + it + 1
+                        //新数据会全部加入，旧数据会只刷新部分UI，刷新UI的效率会极高
+                        list.add(MessageListBean(id = number, name = "${number}", age = number))
+                    }
+                } else {
+                    //模拟一次旧数据在后方
+                    reveredNumber = if (reveredNumber == -4) {
+                        +1
+                    } else {
+                        -4
+                    }
+                    repeat(10){
+                        val number = ((pageNo - 1) * pageSize) + it + reveredNumber
+                        //新数据会全部加入，旧数据会只刷新部分UI，刷新UI的效率会极高
+                        list.add(MessageListBean(id = number, name = "${number + Random.nextInt(30)}", age = number))
+                    }
                 }
             }
             _messageEvent.emit(list)
