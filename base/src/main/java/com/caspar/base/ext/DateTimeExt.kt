70a17550647f23d41614c134dbb54741f24981ed
CasparXL <Caspar_XL@163.com>
2023-01-09 19:08:22 +0800

    优化时间戳转换工具类

--- base/src/main/java/com/caspar/base/ext/DateTimeExt.kt
+++ base/src/main/java/com/caspar/base/ext/DateTimeExt.kt
@@ -1,10 +1,7 @@
 package com.caspar.base.ext
 
 import java.text.SimpleDateFormat
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZoneOffset
+import java.time.*
 import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalAccessor
 import java.util.*
@@ -19,11 +16,25 @@ import java.util.*
  *  根据字符串的时间转换毫秒时间戳,格式不正确时返回-1
  *  @param format 时间的格式，默认是按照yyyy-MM-dd HH:mm:ss来转换，如果您的格式不一样，则需要传入对应的格式
  */
-fun String.timeFormatMillis(format: String = "yyyy-MM-dd HH:mm:ss"): Long {
+fun String?.timeFormatMillis(format: String = "yyyy-MM-dd HH:mm:ss"): Long {
     return try {
         val dtfInput = DateTimeFormatter.ofPattern(format, Locale.getDefault())
-        LocalDateTime.parse(this, dtfInput)
-            .toInstant(ZoneOffset.of(ZoneOffset.systemDefault().normalized().id)).toEpochMilli()
+        LocalDateTime.parse(this ?: "", dtfInput)
+            .toInstant(ZoneOffset.systemDefault().rules.getOffset(Instant.now())).toEpochMilli()
+    } catch (e: java.lang.Exception) {
+        this.timeFormatMillisForData(format)
+    }
+}
+
+/**
+ *  根据字符串的时间转换毫秒时间戳,格式不正确时返回-1
+ *  @param format 时间的格式，默认是按照yyyy-MM-dd HH:mm:ss来转换，如果您的格式不一样，则需要传入对应的格式
+ */
+private fun String?.timeFormatMillisForData(format: String = "yyyy-MM-dd HH:mm:ss"): Long {
+    return try {
+        val dtfInput = DateTimeFormatter.ofPattern(format, Locale.getDefault())
+        LocalDate.parse(this ?: "", dtfInput).atStartOfDay()
+            .toInstant(ZoneOffset.systemDefault().rules.getOffset(Instant.now())).toEpochMilli()
     } catch (e: java.lang.Exception) {
         -1
     }
@@ -32,10 +43,10 @@ fun String.timeFormatMillis(format: String = "yyyy-MM-dd HH:mm:ss"): Long {
 /**
  * 根据时间戳转换字符串时间
  */
-fun Long.timeFormatDate(format: String = "yyyy-MM-dd HH:mm:ss"): String {
+fun Long?.timeFormatDate(format: String = "yyyy-MM-dd HH:mm:ss"): String {
     val time: String = try {
         val dtfInput = DateTimeFormatter.ofPattern(format, Locale.getDefault())
-        LocalDateTime.ofInstant(Instant.ofEpochMilli(this), ZoneId.systemDefault())
+        LocalDateTime.ofInstant(Instant.ofEpochMilli(this ?: -1), ZoneId.systemDefault())
             .format(dtfInput)
     } catch (e: java.lang.Exception) {
         ""
@@ -46,8 +57,8 @@ fun Long.timeFormatDate(format: String = "yyyy-MM-dd HH:mm:ss"): String {
 /**
  * 根据时间戳获取事件对象，从而拿到当天的对象
  */
-fun Long.getLocalDataTime(): LocalDateTime {
-    return LocalDateTime.ofInstant(Instant.ofEpochMilli(this), ZoneId.systemDefault())
+fun Long?.getLocalDataTime(): LocalDateTime {
+    return LocalDateTime.ofInstant(Instant.ofEpochMilli(this ?: -1L), ZoneId.systemDefault())
 }
 
 /**
