c62c66b41c0baeb61689b187ee0611329a2ca4e1
xionglang <976301251@qq.com>
2023-07-28 10:42:53 +0800

    添加构建注释

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,8 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    //RepositoriesMode.PREFER_PROJECT : 解析依赖库时 , 优先使用本地仓库 , 本地仓库没有该依赖 , 则使用远程仓库 ;
+    //RepositoriesMode.FAIL_ON_PROJECT_REPOS : 解析依赖库时 , 强行使用远程仓库 , 不管本地仓库有没有该依赖库 ;
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
